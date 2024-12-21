ee20c312f69a3f080dfa5cb5a56601c7ce7fb738
hosenA51 <hosenahmed2552@gmail.com>
2024-12-21 17:39:01 +0600

    implement pagination

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nweo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,9 +28,33 @@ async function run() {
 
     const productCollection = client.db('emaJohn').collection('products');
 
-    app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
+    app.get('/products', async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+
+      console.log('pagination query', page, size)
+      const result = await productCollection.find()
+      .skip(page * size)
+      .limit(size)
+      .toArray();
+      res.send(result);
+    })
+
+    app.post('/productByIds', async(req, res)=>{
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id))
+      const query = {
+        _id: {
+          $in: idsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/productsCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
     })
 
     // Send a ping to confirm a successful connection
@@ -44,10 +68,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
+app.get('/', (req, res) => {
+  res.send('john is busy shopping')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
 })
