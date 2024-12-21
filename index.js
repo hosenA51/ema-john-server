761b80bdaf88eec8019c64feb3c1f6235f2f75b1
hosenA51 <hosenahmed2552@gmail.com>
2024-12-21 14:08:39 +0600

    basic project setup

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nweo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,7 +26,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products');
+    const productCollection = client.db('emaJohn').collection('products');
 
     app.get('/products', async(req, res) => {
         const result = await productCollection.find().toArray();
